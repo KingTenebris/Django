a5d504a942f599971ee8f0e96c1e85e98f1f4182
LordTenebris <113670625+KingTenebris@users.noreply.github.com>
2023-10-09 12:09:49 +0200

    django

--- FirstProject/FirstApp/views.py
+++ FirstProject/FirstApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hej detta är mitt response på din request")
+    return HttpResponse("<h1>Hej Philip, jag gjorde det</h1>")
