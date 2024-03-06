f1297bcd6ce29f6eb76679d19fdb523c8052b449
LordTenebris <113670625+KingTenebris@users.noreply.github.com>
2024-03-06 15:43:12 +0100

    crud v.2

--- MyGames/Game/urls.py
+++ MyGames/Game/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('')
+    path('', views.home, name="home"),
+    path('AddGame', views.addGame, name="addGame")
 ]
--- MyGames/Game/views.py
+++ MyGames/Game/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+def addGame(request):
+    if request.method == "POST":
+        gameName = request.POST.get ("gameName")
+        gameYear = request.POST.get ("gameYear")
+        gamePhase = request.POST.get ("gamePhase")
+        gameDeveloper = request.POST.get ("gameDeveloper")
+        gamePlatform = request.POST.get ("gamePlatform")
+        gameCategory = request.POST.get ("gameCategory")
+
+    return render(request, "addGame.html")
